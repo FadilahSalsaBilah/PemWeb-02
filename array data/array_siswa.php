a04c6a7831249b75aeb11d9214fd348db0f2d087
Fadilah Salsa Bilah <114551873+dilahnf@users.noreply.github.com>
2023-03-02 17:29:56 +0700

    Add files via upload

---
+++ array data/array_siswa.php
@@ -0,0 +1,76 @@
+<?php
+$ns1 = ['id'=>1,'nim'=>'01101','uts'=>80,'uas'=>84,'tugas'=>78];
+$ns2 = ['id'=>2,'nim'=>'01121','uts'=>70,'uas'=>50,'tugas'=>68];
+$ns3 = ['id'=>3,'nim'=>'01130','uts'=>60,'uas'=>86,'tugas'=>70];
+$ns4 = ['id'=>4,'nim'=>'01134','uts'=>90,'uas'=>91,'tugas'=>82];
+
+$ar_nilai = [$ns1,$ns2,$ns3,$ns4];
+?>
+
+<html>
+  <head>
+    <!-- Menggunakan Internal CSS -->
+    <style>
+        body {
+            background-color: lavender;
+        }
+
+        table {
+            border: 1px solid;
+            width: 100%;
+            border-collapse: collapse;
+            color: #444;
+            font-family: sans-serif;
+        }
+
+        table tr th {
+            background: mediumpurple;
+            color: #fff;
+            font-weight: normal;
+        }
+        
+        table,
+        th,
+        td {
+            padding: 8px 20px;
+            text-align: center;
+        }
+        
+        table tr:hover {
+            background-color: lavenderblush;
+        }
+    </style>
+    <!-- End CSS -->
+  </head>
+  <body>
+    <center><h1>Daftar Nilai Siswa</h1></center>
+    <table>
+      <thead>
+        <tr>
+          <th>No</th>
+          <th>NIM</th>
+          <th>UTS</th>
+          <th>UAS</th>
+          <th>Tugas</th>
+          <th>Nilai Akhir</th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php
+        $nomor=1;
+        foreach($ar_nilai as $ns){
+        echo '<tr><td>' . $nomor . '</td>';
+        echo '<td>' . $ns['nim'] . '</td>';
+        echo '<td>' . $ns['uts'] . '</td>';
+        echo '<td>' . $ns['uas'] . '</td>';
+        echo '<td>' . $ns['tugas'] . '</td>';
+        $nilai_akhir = ($ns['uts'] + $ns['uas'] + $ns['tugas'])/3;
+        echo '<td>'.number_format($nilai_akhir,2,',','.').'</td>';
+        echo '<tr/>';
+        $nomor++;
+        }
+        ?>
+      </tbody>
+    </table>
+  </body>
+</html>
