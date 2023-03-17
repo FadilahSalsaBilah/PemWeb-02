3c7672c7be74e5057fcf9da9468e4013ec58ccd5
Fadilah Salsa Bilah <114551873+ChaeyoungDila@users.noreply.github.com>
2023-03-17 18:31:25 +0700

    Add files via upload

---
+++ praktikum 04/function.php
@@ -0,0 +1,12 @@
+<?php
+include "rumus.php";
+
+luasLingkaran(15);
+
+echo "<br>";
+
+luasPersegi(50);
+
+echo "<br>";
+
+luasSegitiga(10, 40);
---
+++ praktikum 04/rumus.php
@@ -0,0 +1,17 @@
+<?php
+
+function luasLingkaran($jarijari)
+{
+    define('PHI', 3.14);
+    echo PHI * $jarijari * $jarijari;
+}
+
+function luasPersegi($sisi)
+{
+    echo $sisi * $sisi;
+}
+
+function luasSegitiga($alas, $tinggi)
+{
+    echo 1/2 * $alas * $tinggi;
+}
